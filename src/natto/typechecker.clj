2e257e90eb7c9b858b07e0755e99f78258f48818
Egydio Pacheco <egydio.neto@quintoandar.com.br>
2023-10-17 07:31:22 -0300

    chore: add type environment(Γ) and extend function
    Γ will act as the type environment where the typechecker function (wip)
    will look for functions param type annotations and returns types

--- src/natto/typechecker.clj
+++ src/natto/typechecker.clj
@@ -3,6 +3,25 @@
 
 (def ^:dynamic *enable-static-check* false)
 
+
+(def ^:private Γ 
+  "This defines Γ, which is the type environment"
+  (atom {}))
+
+(defn extend-env
+  "Extends the type environment with a pair of symbol/type
+
+  Example: (extend-env some-symbol {:ret ret-type
+                                    :args args-type})
+  "
+  [symbol type]
+  (swap! Γ assoc symbol type))
+
+(defn lookup-type
+  "Look up for a symbol in the type environment"
+  [symbol]
+  (get @Γ symbol))
+
 (defn toggle-static-checking []
   (if *enable-static-check*
     (do
