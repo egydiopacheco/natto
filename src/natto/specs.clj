016825b46f87605013db00db4a9cd23058cd0ab9
Egydio Pacheco <egydio.neto@quintoandar.com.br>
2023-10-17 04:03:36 -0300

    chore: add some specs
    The approach for refined types is definetly not scalable.
    
    The other specs checkings have flaws. But I'm moving on with this for
    now, just for experimenting some applications

--- src/natto/specs.clj
+++ src/natto/specs.clj
@@ -1,7 +1,32 @@
 (ns natto.specs
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.core :as core]
+            [clojure.spec.alpha :as s]))
 
-(s/def ::typeCheck #(let [type (:type %)
+(s/def ::core-type #(let [type (:type %)
                           anno (:anno %)]
                       (or (isa? type anno)
                           (isa? anno type))))
+
+(s/def ::vec-of-type
+  (fn [{:keys [value anno]}]
+    (and (vector? value)
+         (every? #(isa? (type %) anno) value))))
+
+(s/def ::map-of-type
+  (fn [{:keys [value val-type]}]
+    (and (map? value)
+         (every? #(isa? (type (val %)) val-type) value))))
+
+(s/def ::refined-type 
+  (fn [{:keys [value anno]}]
+    (case anno
+      :positive-integer (and (integer? value) (> value 0))
+      :negative-integer (and (integer? value) (< value 0))
+      :zero             (and (integer? value) (= value 0))
+      false)))
+
+(s/def ::type-check
+  (s/or :core     ::core-type
+        :seq      ::vec-of-type
+        :map      ::map-of-type
+        :refined  ::refined-type))
